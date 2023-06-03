78d665ddaaf88afb016407f1c57ffece7245852e
Youjeong Park <pyj0695@gmail.com>
2023-06-03 12:49:17 +0900

    feat : nest CRUD example

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch , Delete} from '@nestjs/common';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    constructor(private readonly catsService : CatsController){
+
+    }
+    
+    // cats / 
+    @Get()
+    getAllCat(){
+        return 'all cat';
+    }
+
+    @Get(':id')
+    getOneCat(){
+        return 'one cat';
+    }
+
+    @Post()
+    createCat(){
+        return 'create cat';
+    }
+
+    @Put(':id')
+    updateCat(){
+        return 'update cat';
+    }
+
+    @Patch(':id')
+    updatePartialCat(){
+        return; 
+    }
+
+    @Delete(':id')
+    deleteCat(){
+        return 'delete service'; 
+    }
+
+}
